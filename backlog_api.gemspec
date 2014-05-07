918552b642357909aef093b932f90411132eced6
shibata <t.shibata@teraoka-sp.com>
2014-05-07 21:03:10 +0900

    コマンドライン引数取れるようにしました

--- backlog_api.gemspec
+++ backlog_api.gemspec
@@ -8,16 +8,17 @@ Gem::Specification.new do |spec|
   spec.version       = BacklogApi::VERSION
   spec.authors       = ["shibata"]
   spec.email         = ["t.shibata@teraoka-sp.com"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
+  spec.description   = %q{backlogのバックログを叩くよ}
+  spec.summary       = %q{backlogのバックログを叩くよ。まずはチケットを作れるようにしたよ。}
   spec.homepage      = ""
-  spec.license       = "MIT"
+  # spec.license       = "MIT"
 
   spec.files         = `git ls-files`.split($/)
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
 
-  spec.add_development_dependency "bundler", "~> 1.3"
+  spec.add_development_dependency "bundler"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "thor"
 end
--- lib/backlog_api.rb
+++ lib/backlog_api.rb
@@ -1,5 +1,52 @@
+# -*- coding: utf-8 -*-
 require "backlog_api/version"
+require 'xmlrpc/client' 
 
 module BacklogApi
-  # Your code goes here...
-end
+  class Client
+
+    # 初期化
+    def initialize
+      # 環境変数をセットしてね
+      @host = ENV["HOST"]
+      @user = ENV["USER"]
+      @password = ENV["PASSWORD"]
+
+      path = '/XML-RPC'
+      port = '443'
+      proxy_host = nil
+      proxy_port = nil
+      use_ssl = true
+      timeout = 60 
+      
+      @client = XMLRPC::Client.new(host, path, port, proxy_host, proxy_port, user, password, use_ssl, timeout)
+    end
+    # アクセッサ
+    attr_accessor :host, :user, :password, :client
+
+    # タイムラインを取得
+    def get_timeline    
+      @client.call('backlog.getTimeline')
+    end
+
+    # 課題追加
+    def create_issue(opt = {})
+      @client.call(
+        'backlog.createIssue', {
+          projectId: ENV["PROJECT_ID"],
+          assignerId: ENV["USER"],
+          summary: opt[:summary],
+        }
+      )
+    end
+
+    # プロジェクト取得
+    def get_projects
+      @client.call('backlog.getProjects')
+    end
+
+
+
+
+  end # Client
+end # BacklogApi
