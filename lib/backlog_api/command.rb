d474727901104ebd1ab46ec556e2a0c77ab2acff
shibata <t.shibata@teraoka-sp.com>
2014-05-11 20:20:06 +0900

    バグ修正

--- lib/backlog_api/command.rb
+++ lib/backlog_api/command.rb
@@ -2,23 +2,11 @@
 module BacklogApi
   class Command < Thor
 
-    
-    ##################################################################################
-    # サブコマンドの説明
-    ##################################################################################
-    # desc "get_timeline", "タイムラインを取得するよ"
-
-
-
-
-    ##################################################################################
-    # サブコマンド定義
-    ##################################################################################
     # まとめて定義
     # 例: $ backlog get_timeline
     BacklogApi::API_METHODS.keys.uniq.each do |api_method|
       # オプション
-      # とりあえず必須パラメータだけパラメータに指定できるようにした
+      # とりあえず必須パラメータだけオプションに指定できるようにした
       BacklogApi::API_METHODS[api_method]["required"].each do |key|
         method_option( key,
           type: :string,
@@ -30,6 +18,7 @@ module BacklogApi
       
       # 説明(必須)
       desc api_method.underscore, (BacklogApi::API_METHODS[api_method]['desc'].presence || api_method)
+
       # サブコマンド定義
       define_method api_method.underscore, &->(params = nil) do
         response = Client.new.send api_method.underscore
