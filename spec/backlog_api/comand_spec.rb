a2ffc5356e793c212baaa874e128d59a5295e4b4
shibata <t.shibata@teraoka-sp.com>
2014-05-12 23:07:46 +0900

    テスト追加

--- spec/backlog_api/comand_spec.rb
+++ spec/backlog_api/comand_spec.rb
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 require_relative 'spec_helper'
-
+# とっぷもじゅーる
 describe BacklogApi do
+  describe Command do
+  end
 end
--- spec/backlog_api/const_spec.rb
+++ spec/backlog_api/const_spec.rb
@@ -1,5 +1,9 @@
-# -*- coding: utf-8 -*-
 require_relative 'spec_helper'
 
 describe BacklogApi do
+  describe Const do
+  end
 end
+
+
+
