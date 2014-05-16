566663dd020423d10de2ec335a7ea68e87dcafb6
shibata <t.shibata@teraoka-sp.com>
2014-05-16 09:44:37 +0900

    とりあえずcommit

--- spec/lib/backlog_api/client_spec.rb
+++ spec/lib/backlog_api/client_spec.rb
@@ -8,22 +8,195 @@ module BacklogApi
   describe Client do
     
     subject { Client.new }
-    
-    describe '#get_timeline' do
-      it { expect(subject.get_timeline).to be_a(Array) }
-    end
+    let(:url) { 'https://%s%s' % [subject.host, Client::PATH]} # 使いまわし
+    let(:opt) { {record: :new_episodes} }
+    let!(:name) {example.metadata[:full_description].split(/\s+/, 2).join("/").underscore.gsub(/[^\w\/]+/, "_")} # 毎回let
 
     
-    BacklogApi::API_METHODS.keys do |k, v|
-      describe '#%s' % k.underscore do
-        it do
-          required
-          opt
-          response subject.send k.underscore
-          expect().to be_a(Array)
+    describe '#get_projects' do
+      it do
+        VCR.use_cassette(name, opt) do # vcrのキャッシュ
+          expect(subject.get_projects).to be_a(Array)
         end
       end
     end
+
+    describe "#get_project" do
+      it do
+        # request_body = '<data projectId="projectId" />'
+        # response_body = {
+        #   "use_parent_child_issue" => true,
+        #   "id" => 1,
+        #   "text_formatting_rule" => "backlog",
+        #   "archived" => false,
+        #   "name" => "name",
+        #   "url" => "url",
+        #   "key" => "key"}          
+        # WebMock
+        #   .stub_request(:post, url) # url
+        #   .with(body: request_body, content_type: 'application/xml') # request
+        #   .to_return(status: 200, body: response_body, headers: {}) # response
+        # expect(subject.get_project("projectId" => "projectID")).to be_a(Hash) # webmock
+        
+        VCR.use_cassette(name, opt) do # vcrのキャッシュ
+          expect(subject.get_project("2014YC")).to be_a(Hash) # webmock
+        end
+      end
+    end
+
+    # describe "#get_components" do
+    #   it do
+    #             pending
+    #     request_body = 1
+    #     response_body = [
+    #       {"id"=>2, "name"=>"name1"},
+    #       {"id"=>3, "name"=>"name2"}
+    #     ]
+    #     WebMock
+    #       .stub_request(:post, url) # url
+    #       .with(body: request_body, headers: {}) # request
+    #       .to_return(status: 200, body: response_body, headers: {}) # response
+    #     expect(subject.get_components(1)).to be_a(Array) # webmock
+    #   end
+    # end
+
+
+
+
+
+    # getComponents:
+    #   type: scalar
+    #   required: [projectId]
+    #   desc: 'プロジェクトのカテゴリを返します。'
+    # getVersions:
+    #   type: scalar
+#   required: [projectId]
+#   desc: 'プロジェクトの発生バージョン/マイルストーンを返します。'
+# getUsers:
+#   type: scalar
+#   required: [projectId]
+#   desc: 'プロジェクトの参加メンバーを返します。'
+# getIssueTypes:
+#   type: scalar
+#   required: [projectId]
+#   desc: 'プロジェクトの種別を返します。'
+# getIssue:
+#   type: scalar
+#   required: [issueKey]
+#   desc: '課題キーを指定して、課題を取得します。'
+# getIssue:
+#   type: scalar
+#   required: [issueId]
+#   desc: '課題IDを指定して、課題を取得します。'
+# getComments:
+#   type: scalar
+#   required: [issueId]
+#   desc: '課題のコメントを返します。'
+# countIssue:
+#   type: struct
+#   required: [projectId]
+#   desc: '指定した条件に該当する課題件数を返します。'
+# findIssue:
+#   type: struct
+#   required: [projectId]
+#   desc: '指定した条件に該当する課題を返します。'
+# createIssue:
+#   type: struct
+#   required: [projectId, summary]
+#   desc: '課題を追加します。'
+# updateIssue:
+#   type: struct
+#   required: [key]
+#   desc: '課題を更新します。'
+# switchStatus:
+#   type: struct
+#   required: [key, statusId]
+#   desc: '課題の状態を変更します。'
+# addComment:
+#   type: struct
+#   required: [key, content]
+#   desc: '課題にコメントを追加します。'
+# addIssueType:
+#   type: struct
+#   required: [project_id, name, color]
+#   desc: 'プロジェクトの課題種別を追加します。'
+# updateIssueType:
+#   type: struct
+#   required: [id, name, color]
+#   desc: 'プロジェクトの課題種別を更新します。'
+# deleteIssueType:
+#   type: struct
+#   required: [id, substitute_id]
+#   desc: 'プロジェクトの課題種別を削除します。'
+# addVersion:
+#   type: struct
+#   required: [project_id, name]
+#   desc: 'プロジェクトの発生バージョン/マイルストーンを追加します。'
+# updateVersion:
+#   type: struct
+#   required: [id, name]
+#   desc: 'プロジェクトの発生バージョン/マイルストーンを更新します。'
+# deleteVersion:
+#   type: struct
+#   required: [id]
+#   desc: 'ロジェクトの発生バージョン/マイルストーンを削除します。'
+# addComponent:
+#   type: struct
+#   required: [project_id, name]
+#   desc: 'プロジェクトのカテゴリを追加します。追加に成功した場合は、'
+# updateComponent:
+#   type: struct
+#   required: [id, name]
+#   desc: 'プロジェクトのカテゴリを更新します。'
+# deleteComponent:
+#   type: struct
+#   required: [id]
+#   desc: 'プロジェクトのカテゴリを削除します。'
+# getTimeline:
+#   type: struct
+#   required: []
+#   desc: '参加プロジェクトすべての課題の更新情報を配列で返します(最大50件)。'
+# getProjectSummary:
+#   type: scalar
+#   required: [projectId]
+#   desc: 'プロジェクト状況を取得します。'
+# getProjectSummaries:
+#   type: null
+#   required: []
+#   desc: '全ての参加プロジェクト状況を取得します。'
+# getUser:
+#   type: scalar
+#   required: [id]
+#   desc: 'ユーザID(数値またはログインID)を指定して、ユーザを取得します。'
+# getUserIcon:
+#   type: scalar
+#   required: [id]
+#   desc: 'ユーザID(数値またはログインID)を指定して、ユーザのアイコン画像を取得します。'
+# getActivityTypes:
+#   type: null
+#   required: []
+#   desc: '課題の更新情報の種別一覧を取得します。'
+# getStatuses:
+#   type: null
+#   required: []
+#   desc: '課題の状態一覧を取得します。'
+# getResolutions:
+#   type: null
+#   required: []
+#   desc: '課題の完了理由一覧を取得します。'
+# getPriorities:
+#   type: null
+#   required: []
+#   desc: '課題の優先度一覧を取得します。'
+# getCustomFields:
+#   type: struct
+#   required: [projectId]
+#   desc: 'プロジェクトに登録してあるカスタム属性の情報を取得します。'
+# getChildIssues:
+#   type: struct
+#   required: [parent_issue_id]
+#   desc: '指定した親課題のIDの子課題を返します。'
+
     
   
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,17 +17,17 @@ RSpec.configure do |config|
   VCR.configure do |c|
     c.cassette_library_dir = 'spec/vcr_cassettes' # VCRで記録するYAMLファイルのパス
     c.hook_into :webmock # webmockをhookとする
-    # c.allow_http_connections_when_no_cassette = true  # VCRブロック外のHTTP通信は許可する
+    c.allow_http_connections_when_no_cassette = true  # VCRブロック外のHTTP通信は許可する
   end
 
   # VCR.use_cassetteを使わなくて済む。Specのdescribeを元にVCRのファイル名を生成する。"spec/lib/backlog_apiのClient#get_timeline"であれば"spec/vcr_cassettes/backlog_api/client_get_timeline.yml"を作成
-  config.around(:each) do |example|
-    options = example.metadata[:vcr] || {}
-    if options[:record] == :skip 
-      VCR.turned_off(&example)
-    else
-      name = example.metadata[:full_description].split(/\s+/, 2).join("/").underscore.gsub(/[^\w\/]+/, "_")
-      VCR.use_cassette(name, options, &example)
-    end
-  end
+  # config.around(:each) do |example| # aroundフック
+  #   options = example.metadata[:vcr] || {}
+  #   if options[:record] == :skip 
+  #     VCR.turned_off(&example)
+  #   else
+  #     name = example.metadata[:full_description].split(/\s+/, 2).join("/").underscore.gsub(/[^\w\/]+/, "_") # descからファイル名を取得
+  #     VCR.use_cassette(name, options, &example) # コレで囲むよー
+  #   end
+  # end
 end
