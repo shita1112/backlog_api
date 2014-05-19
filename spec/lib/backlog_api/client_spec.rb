48e31fa76a3ccbd6f378426aa033a0841ef82c6f
shibata <t.shibata@teraoka-sp.com>
2014-05-20 03:00:35 +0900

    [WIP]テスト整理したよ

--- spec/lib/backlog_api/client_spec.rb
+++ spec/lib/backlog_api/client_spec.rb
@@ -8,41 +8,288 @@ module BacklogApi
   describe Client do
     
     subject { Client.new }
-    let(:url) { 'https://%s%s' % [subject.host, Client::PATH]} # 使いまわし
-    let(:opt) { {record: :new_episodes} }
-    let!(:name) {example.metadata[:full_description].split(/\s+/, 2).join("/").underscore.gsub(/[^\w\/]+/, "_")} # 毎回let
 
+    # letはitの度に生成されるよ。letは遅延評価でlet!は即評価だよ
+    let(:url) { 'https://%s%s' % [subject.host, Client::PATH]} 
     
+    # for vcr
+    let(:opt) { {record: :new_episodes} }
+    let(:path) {example.metadata[:full_description].split(/\s+/, 2).join("/").underscore.gsub(/[^\w\/]+/, "_")}
+
+    # for backlog_api's key
+    let(:project_key) {ENV['BACKLOG_PROJECTKEY'] }
+    let(:project_id) {ENV['BACKLOG_PROJECTID'].to_i}
+    let(:issue_key) {ENV['BACKLOG_ISSUEKEY']} 
+    let(:issue_id) {ENV['BACKLOG_ISSUEID'].to_i} 
+    let(:summary) {ENV['BACKLOG_SUMMARY']} 
+    let(:key) {ENV['BACKLOG_KEY']} 
+    let(:status_id) {ENV['BACKLOG_STATUSID'].to_i} 
+    let(:name) {ENV['BACKLOG_NAME']} 
+    let(:color) {ENV['BACKLOG_COLOR']} 
+    let(:substitute_id) {ENV['BACKLOG_SUBSTITUTE_ID'].to_i} 
+    let(:id) {ENV['BACKLOG_ID'].to_i} 
+    let(:parent_issue_id) {ENV['BACKLOG_PARENTISSUEID'].to_i} 
+    let(:project_name) {ENV['BACKLOG_PROJECTNAME']} 
+    let(:project_id) {ENV['BACKLOG_PROJECTID'].to_i} 
+    let(:category_name) {ENV['BACKLOG_CATEGORYNAME']} 
+    let(:project_id2) {ENV['BACKLOG_PROJECTID2'].to_i} 
+    let(:user_id) {ENV['BACKLOG_USERID'].to_i} 
+
+    METHOD = 'backlog.%s'
+
     describe '#get_projects' do
       it do
-        VCR.use_cassette(name, opt) do # vcrのキャッシュ
-          expect(subject.get_projects).to be_a(Array)
+        VCR.use_cassette(path, opt) do # vcrのキャッシュ
+          expect(subject.get_projects.tapp).to be_a(Array)
         end
       end
     end
 
-    describe "#get_project" do
+    describe '#get_project' do
       it do
-        # request_body = '<data projectId="projectId" />'
-        # response_body = {
-        #   "use_parent_child_issue" => true,
-        #   "id" => 1,
-        #   "text_formatting_rule" => "backlog",
-        #   "archived" => false,
-        #   "name" => "name",
-        #   "url" => "url",
-        #   "key" => "key"}          
-        # WebMock
-        #   .stub_request(:post, url) # url
-        #   .with(body: request_body, content_type: 'application/xml') # request
-        #   .to_return(status: 200, body: response_body, headers: {}) # response
-        # expect(subject.get_project("projectId" => "projectID")).to be_a(Hash) # webmock
-        
-        VCR.use_cassette(name, opt) do # vcrのキャッシュ
-          expect(subject.get_project("2014YC")).to be_a(Hash) # webmock
+        VCR.use_cassette(path, opt) do # vcrのキャッシュ
+          expect(subject.get_project(project_key).tapp).to be_a(Hash)
         end
       end
     end
+
+    describe '#get_components' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.get_components(project_id).tapp).to be_a(Array)
+        end
+      end
+    end
+
+    describe '#get_versions' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.get_versions(project_id).tapp).to be_a(Array)
+        end
+      end
+    end
+
+    describe '#get_users' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.get_users(project_id).tapp).to be_a(Array)
+        end
+      end
+    end
+
+    describe '#get_issue_types' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.get_issue_types(project_id).tapp).to be_a(Array)
+        end
+      end
+    end
+
+    describe '#get_issue' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.get_issue(project_id).tapp).to be_a(Hash)
+        end
+      end
+    end
+
+    describe '#get_users' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.get_users(project_id).tapp).to be_a(Array)
+        end
+      end
+    end
+
+    describe '#get_issue' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.get_issue(issue_id).tapp).to be_a(Hash)
+        end
+      end
+    end
+
+    describe '#get_comments' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.get_comments(issue_id).tapp).to be_a(Array)
+        end
+      end
+    end
+
+    describe '#count_issue' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.count_issue(projectId: project_id).tapp).to be_a(Integer)
+        end
+      end
+    end
+
+    describe '#find_issue' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.find_issue(projectId: project_id).tapp(&:first)).to be_a(Array)
+        end
+      end
+    end
+
+    describe '#create_issue' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(
+            subject.create_issue(
+              projectId: project_id,
+              summary: summary,
+              ).tapp
+            ).to be_a(Hash)
+        end
+      end
+    end
+
+    describe '#update_issue' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.update_issue(key: key).tapp).to be_a(Hash)
+        end
+      end
+    end
+
+    describe '#switch_status' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.switch_status(key: key, statusId: status_id).tapp).to be_a(Hash)
+        end
+      end
+    end
+
+    describe '#add_issue_type' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(
+            subject.add_issue_type(
+              project_id: project_id,
+              name: name,
+              color: color, 
+              ).tapp
+            ).to be_a(Hash)
+        end
+      end
+    end
+
+    describe '#update_issue_type' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(
+            subject.update_issue_type(
+              id: id,
+              name: name,
+              color: color, 
+              ).tapp              
+            ).to be_a(Hash)
+        end
+      end
+    end
+
+
+    describe '#delete_issue_type' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(
+            subject.delete_issue_type(
+              id: id,
+              ).tapp              
+            ).to be_a(Hash)
+        end
+      end
+    end
+
+
+    describe '#add_version' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(
+            subject.add_version(
+              project_id: project_id,
+              name: project_name
+              ).tapp
+            ).to be_a(Hash)
+        end
+      end
+    end
+
+
+    describe '#update_version' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.update_version(id: project_id, name: project_name).tapp).to be_a(Hash)
+        end
+      end
+    end
+
+    describe '#get_timeline' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.get_timeline.tapp(&:first)).to be_a(Array)
+        end
+      end
+    end
+
+
+    describe '#get_project_summary' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.get_project_summary(project_id).tapp).to be_a(Hash)
+        end
+      end
+    end
+
+    describe '#get_statuses' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.get_statuses.tapp).to be_a(Array)
+        end
+      end
+    end
+
+    describe '#get_resolutions' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.get_resolutions.tapp).to be_a(Array)
+        end
+      end
+    end
+
+    describe '#get_priorities' do
+      it do
+        VCR.use_cassette(path, opt) do
+          expect(subject.get_priorities.tapp).to be_a(Array)
+        end
+      end
+    end
+
+ 
+
+
+    # describe "#get_project" do
+    #   it do
+    #     request_body = '<data projectId="projectId" />'
+    #     response_body = {
+    #       "use_parent_child_issue" => true,
+    #       "id" => 1,
+    #       "text_formatting_rule" => "backlog",
+    #       "archived" => false,
+    #       "name" => "name",
+    #       "url" => "url",
+    #       "key" => "key"}          
+    #     WebMock
+    #       .stub_request(:post, url) # url
+    #       .with(body: request_body, content_type: 'application/xml') # request
+    #       .to_return(status: 200, body: response_body, headers: {}) # response
+    #     expect(subject.get_project("projectId" => "projectID")).to be_a(Hash) # webmock
+        
+    #     expect(subject.get_project("2014YC")).to be_a(Hash) # webmock
+    #   end
+    # end
 
     # describe "#get_components" do
     #   it do
@@ -60,207 +307,36 @@ module BacklogApi
     #   end
     # end
 
-
-
-
-
-    # getComponents:
-    #   type: scalar
-    #   required: [projectId]
-    #   desc: 'プロジェクトのカテゴリを返します。'
-    # getVersions:
-    #   type: scalar
-#   required: [projectId]
-#   desc: 'プロジェクトの発生バージョン/マイルストーンを返します。'
-# getUsers:
-#   type: scalar
-#   required: [projectId]
-#   desc: 'プロジェクトの参加メンバーを返します。'
-# getIssueTypes:
-#   type: scalar
-#   required: [projectId]
-#   desc: 'プロジェクトの種別を返します。'
-# getIssue:
-#   type: scalar
-#   required: [issueKey]
-#   desc: '課題キーを指定して、課題を取得します。'
-# getIssue:
-#   type: scalar
-#   required: [issueId]
-#   desc: '課題IDを指定して、課題を取得します。'
-# getComments:
-#   type: scalar
-#   required: [issueId]
-#   desc: '課題のコメントを返します。'
-# countIssue:
-#   type: struct
-#   required: [projectId]
-#   desc: '指定した条件に該当する課題件数を返します。'
-# findIssue:
-#   type: struct
-#   required: [projectId]
-#   desc: '指定した条件に該当する課題を返します。'
-# createIssue:
-#   type: struct
-#   required: [projectId, summary]
-#   desc: '課題を追加します。'
-# updateIssue:
-#   type: struct
-#   required: [key]
-#   desc: '課題を更新します。'
-# switchStatus:
-#   type: struct
-#   required: [key, statusId]
-#   desc: '課題の状態を変更します。'
-# addComment:
-#   type: struct
-#   required: [key, content]
-#   desc: '課題にコメントを追加します。'
-# addIssueType:
-#   type: struct
-#   required: [project_id, name, color]
-#   desc: 'プロジェクトの課題種別を追加します。'
-# updateIssueType:
-#   type: struct
-#   required: [id, name, color]
-#   desc: 'プロジェクトの課題種別を更新します。'
-# deleteIssueType:
-#   type: struct
-#   required: [id, substitute_id]
-#   desc: 'プロジェクトの課題種別を削除します。'
-# addVersion:
-#   type: struct
-#   required: [project_id, name]
-#   desc: 'プロジェクトの発生バージョン/マイルストーンを追加します。'
-# updateVersion:
-#   type: struct
-#   required: [id, name]
-#   desc: 'プロジェクトの発生バージョン/マイルストーンを更新します。'
-# deleteVersion:
-#   type: struct
-#   required: [id]
-#   desc: 'ロジェクトの発生バージョン/マイルストーンを削除します。'
-# addComponent:
-#   type: struct
-#   required: [project_id, name]
-#   desc: 'プロジェクトのカテゴリを追加します。追加に成功した場合は、'
-# updateComponent:
-#   type: struct
-#   required: [id, name]
-#   desc: 'プロジェクトのカテゴリを更新します。'
-# deleteComponent:
-#   type: struct
-#   required: [id]
-#   desc: 'プロジェクトのカテゴリを削除します。'
-# getTimeline:
-#   type: struct
-#   required: []
-#   desc: '参加プロジェクトすべての課題の更新情報を配列で返します(最大50件)。'
-# getProjectSummary:
-#   type: scalar
-#   required: [projectId]
-#   desc: 'プロジェクト状況を取得します。'
-# getProjectSummaries:
-#   type: null
-#   required: []
-#   desc: '全ての参加プロジェクト状況を取得します。'
-# getUser:
-#   type: scalar
-#   required: [id]
-#   desc: 'ユーザID(数値またはログインID)を指定して、ユーザを取得します。'
-# getUserIcon:
-#   type: scalar
-#   required: [id]
-#   desc: 'ユーザID(数値またはログインID)を指定して、ユーザのアイコン画像を取得します。'
-# getActivityTypes:
-#   type: null
-#   required: []
-#   desc: '課題の更新情報の種別一覧を取得します。'
-# getStatuses:
-#   type: null
-#   required: []
-#   desc: '課題の状態一覧を取得します。'
-# getResolutions:
-#   type: null
-#   required: []
-#   desc: '課題の完了理由一覧を取得します。'
-# getPriorities:
-#   type: null
-#   required: []
-#   desc: '課題の優先度一覧を取得します。'
-# getCustomFields:
-#   type: struct
-#   required: [projectId]
-#   desc: 'プロジェクトに登録してあるカスタム属性の情報を取得します。'
-# getChildIssues:
-#   type: struct
-#   required: [parent_issue_id]
-#   desc: '指定した親課題のIDの子課題を返します。'
-
     
-  
-
-
-
-
-
-
-
-
-
-
-
+    describe '#new' do
+      it do
+        expect(subject).to be_an_instance_of Client # instance_of(直接のインスタンス)
+      end
+    end
 
 
     # ====================================================
-    # 後回し
+    # private
     # ====================================================
+    describe '#call' do
+      it do
+        VCR.use_cassette(path, opt) do
+          response = subject.send :call, METHOD % "getProjects"
+          expect(response).to be_a Array
+        end
+      end
+    end
 
+    describe '#login_from_args' do
+      it do
+        subject.send :login_from_args, {user: "a", password: "b", space: "c"}
+        expect(subject.user).to eq "a" # ==
+        expect(subject.password).to eq "b" 
+        expect(subject.space).to eq "c"
+      end
+    end
 
-
-    # # init do
-    # describe '#init do
-    #   it do
-    #     expect(subject).to be_an_instance_of Client # instance_of(直接のインスタンス)
-    #   end
-    # end
-
-
-    # # TODO:
-    # # APIのアクセス
-    # API_METHODS.each do |k, v|
-    #   # 1メソッド
-    #   describe ('#%s' % k).underscore do
-    #     # TODO: 
-    #     it do
-    #       pending
-    #       expect(subject.get_timeline).to be_a v[:return_type].constantize
-    #     end
-    #   end # describe
-    # end 
-    
-
-
-
-
-
-
-
-
-
-
-
-    # ######################################################################
-    # # private
-    # ######################################################################
-    # describe '#call' do
-    #   it do
-    #     response = subject.send :call, "getTimeline"
-    #     expect(response).to be_a Array
-    #   end
-    # end
-
-    # # TODO: だめ
+    # # TODO: だめだー修正だー
     # describe '#login_from_netrc' do
     #   it do
     #     subject.send :login_from_netrc
@@ -288,14 +364,6 @@ module BacklogApi
     #   end
     # end
 
-    # describe '#login_from_args' do
-    #   it do
-    #     subject.send :login_from_args, {user: "a", password: "b", space: "c"}
-    #     expect(subject.user).to eq "a" # ==
-    #     expect(subject.password).to eq "b" 
-    #     expect(subject.space).to eq "c"
-    #   end
-    # end
 
 
 
